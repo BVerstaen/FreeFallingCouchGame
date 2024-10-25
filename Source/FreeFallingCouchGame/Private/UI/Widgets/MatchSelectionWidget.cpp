f936fb8e8b8365c63ebef7070f9962a849063a4b
Brach <alessandrotano@outlook.fr>
2024-10-25 14:40:25 +0200

    Organisation
    Moved bp_warning

---
+++ Source/FreeFallingCouchGame/Private/UI/Widgets/MatchSelectionWidget.cpp
@@ -0,0 +1,43 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/Widgets/MatchSelectionWidget.h"
+
+void UMatchSelectionWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	//Bind delegates and default appearance
+}
+// Called every time the widget is compiled / property changed
+void UMatchSelectionWidget::SynchronizeProperties()
+{
+	Super::SynchronizeProperties();
+/*
+	if(Label)
+		Label->SetText(LabelText);
+
+	if(Grid && ItemWidgetClass)
+	{
+		Grid->ClearChildren();
+		for(int32 x = 0; x < Columns; ++x)
+		{
+			UUserWidget* Widget = CreateWidget<UUserWidget>
+			(GetWorld(), ItemWidgetClass);
+			if(Widget)
+			{
+				UUniformGridSlot* GridSlot = Grid->AddChildToUniformGrid(
+					Widget);
+				GridSlot->SetColumn(Columns);
+				GridSlot->SetRow(1);
+			}
+		}
+	}
+	*/
+}
+
+void UMatchSelectionWidget::TestCallOnPressed(FString NameElementPressed)
+{
+	GEngine->AddOnScreenDebugMessage(-1, 7.f, FColor::Purple, FString::Printf(TEXT("Current Round: %s"), *NameElementPressed));
+
+}
---
+++ Source/FreeFallingCouchGame/Public/UI/Widgets/MatchSelectionWidget.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Characters/PlayerMatchData.h"
+#include "Components/TextBlock.h"
+#include "Components/UniformGridPanel.h"
+#include "Components/UniformGridSlot.h"
+#include "MatchSelectionWidget.generated.h"
+
+/**
+ * 
+ */
+
+// Class is abstract since we want to make instances of the bp created with this instead
+UCLASS(Abstract)
+class UMatchSelectionWidget : public UUserWidget
+{
+	GENERATED_BODY()
+public:
+	//Native construct is more useful than regular cpp constructor for widgets
+	virtual void NativeConstruct() override;
+	virtual void SynchronizeProperties() override;
+
+	UFUNCTION(BlueprintCallable, Category = "MatchSelectionWidget")
+	void TestCallOnPressed(FString NameElementPressed);
+protected:
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Match Data")
+	UPlayerMatchData *PlayerCustomData;
+
+	UPROPERTY(EditAnywhere, Category = "Blueprint Data")
+	TSubclassOf<UUserWidget> ItemWidgetClass = nullptr;
+	UPROPERTY(EditAnywhere, Category = "Blueprint Data", meta=(BindWidget))
+	TObjectPtr<UUniformGridPanel> Grid;
+};
