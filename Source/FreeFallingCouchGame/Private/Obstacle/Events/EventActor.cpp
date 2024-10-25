363cade272cfe1d8cefa3d7a3a50005b4179bc4f
Volod <volodiabussereau@gmail.com>
2024-10-25 17:04:10 +0200

    Event Manager

---
+++ Source/FreeFallingCouchGame/Private/Obstacle/Events/EventActor.cpp
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Obstacle/Events/EventActor.h"
+
+
+// Sets default values
+AEventActor::AEventActor()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AEventActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AEventActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AEventActor::TriggerEvent()
+{
+	
+}
+
---
+++ Source/FreeFallingCouchGame/Public/Obstacle/Managers/EventsManager.h
@@ -0,0 +1,54 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "EventsManager.generated.h"
+
+class AEventActor;
+class AObstacleSpawner;
+
+UCLASS()
+class FREEFALLINGCOUCHGAME_API AEventsManager : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AEventsManager();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+	
+private:
+	AEventActor* GetRandomEvent();
+
+	UFUNCTION()
+	void OnEventEnded(AEventActor* TriggeringActor);
+
+	UPROPERTY(EditAnywhere)
+	AObstacleSpawner* ObstacleSpawner = nullptr;
+
+	UPROPERTY(EditAnywhere)
+	float TimeBetweenEvents = 5.f;
+
+	UPROPERTY()
+	float EventClock = 0.f;
+
+	UPROPERTY()
+	bool EventHappening = false;
+
+	UPROPERTY(EditAnywhere)
+	TArray<AEventActor*> EventsActors;
+
+	UPROPERTY()
+	TArray<AEventActor*> AvailableEventsActors;
+};
